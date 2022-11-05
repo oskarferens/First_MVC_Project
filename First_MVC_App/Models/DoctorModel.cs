3d45ced608d7f86db4c8e7a72cb003258b8feddb
Oskar Ferens <oskar.ferens1@gmail.com>
2022-11-05 18:11:04 +0100

    Assignment 2 done

---
+++ First_MVC_App/Models/DoctorModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace First_MVC_App.Models
+{
+    public class DoctorModel
+    {
+        public static string CheckTemp(string temp)
+        {
+            float temperature = float.Parse(temp);
+
+            if (temperature >= 38)
+                return "You have a fever";
+            else if (temperature <= 35)
+                return "You have hypothermia";
+            else
+                return "You are healthy!";
+        }
+    }
+}
