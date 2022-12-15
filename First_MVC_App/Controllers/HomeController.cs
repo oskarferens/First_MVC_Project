64fca9c1ad9963ad8fd98b1eb2cb8229eb6f6d9c
Oskar Ferens <oskar.ferens1@gmail.com>
2022-12-15 11:08:19 +0100

    Added Birthday and checkAdmin to View, Updated Db.

--- First_MVC_App/Controllers/HomeController.cs
+++ First_MVC_App/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
-﻿using First_MVC_App.Data;
-using First_MVC_App.Models;
-using First_MVC_App.ViewModels;
-using Microsoft.AspNetCore.Authorization;
+﻿
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Data;
 
 namespace First_MVC_App.Controllers
 {
