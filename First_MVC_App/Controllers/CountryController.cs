1a77a0e5fef8c22cd6116398c2aa40a1a0258a4d
Oskar Ferens <75861192+oskarferens@users.noreply.github.com>
2023-01-31 12:40:17 +0100

    some changes

--- First_MVC_App/Controllers/CountryController.cs
+++ First_MVC_App/Controllers/CountryController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
-
+ 
 namespace First_MVC_App.Controllers
 {
     public class CountryController : Controller
